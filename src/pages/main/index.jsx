235090bd270a5e244f4aa4e560fc7a3b6510e4f3
Franco Antonio Matos Ramos <113787760+FrancoAMR@users.noreply.github.com>
2025-11-24 03:44:47 -0500

    Adding hero image

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import '@/styles/main.css';
 import ServicesSection from './components/ServicesSection';
+import mainImage from '@/assets/hero-image.webp';
 
 export default function Main() {
   return (
     <main>
-      <h1>Bienvenido a Yupaninti</h1>
-      <p>Sistema de gestión financiera</p>
+      <div className="main-hero">
+        <img src={mainImage} alt="Main" className="main-image" />
+        <div className="main-content">
+          <h1>Bienvenido a Yupaninti</h1>
+          <p>Sistema de gestión financiera</p>
+        </div>
+      </div>
       <ServicesSection />
     </main>
   );
